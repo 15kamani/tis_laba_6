389a1d8508c2b86864ad3755ad3a769c576a2626
Alina <alinakamanina418@gmail.com>
2023-09-12 17:03:52 +0300

    laba_5

---
+++ 2. Модули/3/app.js
@@ -0,0 +1,4 @@
+const User = require('./user.js');
+
+let eugene = new User('Alina', 32);
+eugene.sayHi();
---
+++ 4. Объект global и глобальные переменные/1/app.js
@@ -0,0 +1,6 @@
+const greeting = require('./greeting');
+
+global.name = 'Alina';
+
+global.console.log(date);
+console.log(greeting.getMessage());
